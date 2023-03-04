4446b85f85f1982989d9244afd989d35d1c5deda
Voth000 <101801239+Voth000@users.noreply.github.com>
2023-03-05 01:15:47 +0700

    huge

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import {GLTFLoader} from "./GLTFLoader.js";
 
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
 
+import {DragControls} from "./DragControls.js";
+
 
 
 
@@ -17,7 +19,12 @@ const loader = new GLTFLoader()
 loader.load('./4.glb', function(glb){
   console.log(glb)
 const root = glb.scene;
-root.scale.set(0.3, 0.3, 0.3)
+root.scale.set(2, 2, 2);
+root.traverse(function(node) {
+    if(node.isMesh)
+    node.castShadow = true;
+    node.receiveShadow = false;
+})
    scene.add(root);
 }, function(xhr){
    console.log(xhr.loaded/xhr.total * 100 + "% loaded")
@@ -26,21 +33,32 @@ root.scale.set(0.3, 0.3, 0.3)
 })
 
 //light
-const light = new THREE.AmbientLight(0xffffff, 0.6)
+const light = new THREE.AmbientLight(0xffffff, 0.8)
 light.position.set(2,10,5)
+light.castShadow = true
 scene.add(light)
 
-const al = new THREE.AmbientLight(0xffffff, 0.25)
-light.position.set(20,-10,-5)
+const al = new THREE.AmbientLight(0xFF00FF, 0.6)
+al.position.set(20,-10,-5)
+al.castShadow = true
 scene.add( al )
 
-const wl = new THREE.DirectionalLight(0xffffff, 1)
-light.position.set(8,3,8)
+const wl = new THREE.DirectionalLight(0xFFFF00, 0.8)
+wl.position.set(8,3,8)
+wl.castShadow = true
 scene.add( wl )
 
+//Create a plane that receives shadows (but does not cast them)
+const planeGeometry = new THREE.PlaneGeometry( 40, 30, 32, 32 );
+const planeMaterial = new THREE.MeshStandardMaterial( { color: 0xFFFFFF } )
+const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+plane.receiveShadow = true;
+plane.rotateX( - Math.PI / 2);
+scene.add( plane );
+///grid
 
-
-
+const grid = new THREE.GridHelper(0,0);
+scene.add( grid );
 //Boiler 
 
 const sizes = {
@@ -50,7 +68,7 @@ const sizes = {
 
 //camera
 const camera = new THREE.PerspectiveCamera(70, sizes.width/sizes.height, 0.1, 1000)
-camera.position.set(1,3,1)
+camera.position.set(0,10,9)
 camera.lookAt(scene.position)
 scene.add(camera)
 
@@ -61,10 +79,16 @@ const renderer = new THREE.WebGL1Renderer({
     alpha: true
 })
 
+///plane
+
+
+
 ///orbit
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.autoRotate = true;
+
+
 
 ///
 
